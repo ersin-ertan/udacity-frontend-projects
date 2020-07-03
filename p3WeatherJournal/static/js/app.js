28e7c9f47b14c9cfcff27cadeda042ac6f389e9a
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2020-07-03 16:37:32 -0400

    Add files via upload

--- p3WeatherJournal/static/js/app.js
+++ p3WeatherJournal/static/js/app.js
@@ -3,27 +3,28 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     addEntryHolder()
+    initPostButton()
 
     // Adds an event listener to an existing HTML button from DOM using Vanilla JS.
     // In the file app.js, the element with the id of generate should have an addEventListener() method called on it, with click as the first parameter, and a named callback function as the second parameter.
     document.getElementById("generate").addEventListener("click", getZipWeather)
+
+});
+
+function initPostButton() {
     const button = createElementWithId("button", "post")
     button.innerText = "POST"
-    button.addEventListener("click", function () {
-        console.log("click")
-        post({ time: Date() })
-    })
+    button.addEventListener("click", function () { post({ time: Date() }) })
     document.body.appendChild(button)
-});
+}
 
 // The client side function should take two arguments, the URL to make a POST to, and an object holding the data to POST.
 async function post(data) {
+
     const response = await fetch("http://localhost:8000/post", {
         method: 'POST',
         credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json', },
         // Body data type must match "Content-Type" header        
         body: JSON.stringify(data),
     });
@@ -32,37 +33,31 @@ async function post(data) {
         const newData = await response.json()
         console.log(newData)
         return newData
-    } catch (error) {
-        console.log("error", error)
-    }
+    } catch (error) { console.log("error", error) }
 }
 
 function getZipWeather() {
 
-    // document.getElementById("zip").value
-    // zip = "47722"
-    // if (parseInt(zip) != NaN && zip.length == 5) {
-    //     console.log("zip: " + zip);
-    //     let url = "http://localhost:8000/weather?" + new URLSearchParams({ zip })
-    //     fetch(url).then(resp => { // There should be an asynchronous function to fetch the data from the app endpoint
-    //         resp.json().then(data => {
-    //             console.log()
-    //             console.log("fetch: " + JSON.stringify(data))
-    //             // Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
-    //             document.getElementById("temp").innerHTML = "Temp: " + data.main.temp
-    // document.getElementById("date").innerHTML = "Date: " + new Date()
+    document.getElementById("zip").value
 
-    //         }).catch(error => {
-    //             alert("Parse error: " + error)
-    //         });
+    if (parseInt(zip) != NaN && zip.length == 5) {
 
-    //     }).catch(error => {
-    //         alert("fetch error: " + error)
-    //     })
-    // }
-    // else {
-    //     alert("default GET zip client query value")
-    // }
+        console.log("zip: " + zip);
+
+        let url = "http://localhost:8000/weather?" + new URLSearchParams({ zip })
+
+        fetch(url).then(resp => { // There should be an asynchronous function to fetch the data from the app endpoint
+            resp.json().then(data => {
+
+                console.log("fetch: " + JSON.stringify(data))
+                // Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
+                document.getElementById("temp").innerHTML = "Temp: " + data.main.temp
+                document.getElementById("date").innerHTML = "Date: " + new Date()
+
+            }).catch(error => { alert("Parse error: " + error) });
+        }).catch(error => { alert("fetch error: " + error) })
+    }
+    else { alert("default GET zip client query value") }
 }
 
 function createElementWithId(tag, id) {
