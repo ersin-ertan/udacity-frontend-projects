f55d5203be4bce1416859dd553fdfeecc990f03f
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2020-07-04 20:53:13 -0400

    Add files via upload

--- p3WeatherJournal/server.js
+++ p3WeatherJournal/server.js
@@ -1,57 +1,70 @@
-// test
-
 const [express, cors, bodyParser, fetch] = ["express", "cors", "body-parser", "node-fetch"].map(require)
+// The body-parser package should be installed and included in the project.
 const openWeatherMapApiKey = process.env.OWMPAK // Create API credentials on OpenWeatherMap.com
 // The personal API Key for OpenWeatherMap API is saved in a named const variable.
-const app = express() // Node and Express should be installed on the local machine. The project file server.js should require express(), and should create an instance of their app using express.
 const PORT = 8000
-let projectData = {} // There should be a JavaScript Object named projectData initiated in the file server.jsto act as the app API endpoint.
+let projectData = {
+    postData: [""],
+} // There should be a JavaScript Object named projectData initiated in the file server.js to act as the app API endpoint.
 const owmEndpoint = "https://api.openweathermap.org/data/2.5/weather" //?zip=" + zip + ",us&appid="
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json()) // The body-parser package should be installed and included in the project.
-app.use(cors()) // The ‘cors’ package should be installed in the project from the command line, required in the project file server.js, and the instance of the app should be setup to use cors().
-app.use(express.static("static")) // The Express app instance should be pointed to the project folder with .html, .css, and .js files.
-app.use(express.json())
+initServer()
 
+function initServer(port = PORT, apiKey = openWeatherMapApiKey,) {
 
-const server = app.listen(PORT, () => {
-    console.log("listening...👂")
-    console.log(openWeatherMapApiKey)
-})
+    app = express() // Node and Express should be installed on the local machine. The project file server.js should require express(), and should create an instance of their app using express.
+    app.use(express.static("static")) // The Express app instance should be pointed to the project folder with .html, .css, and .js files.
+    app.use(express.json())
 
-// There should be a GET route setup on the server side with the first argument as a string naming the route, and the second argument a callback function to return the JS object created at the top of server code.
-app.get('/weather', async function (req, res) {
-    const zip = req.query.zip
-    if (zip != null) {
-        console.log(zip)
-        let url = owmEndpoint + "?" + new URLSearchParams({ zip }) + ",us&appid=" + openWeatherMapApiKey // The API Key variable is passed as a parameter to fetch() .
-        console.log(url)
-        try {
-            let resp = await fetch(url)
-            projectData = await resp.json() // Data is successfully returned from the external API.
-            console.log(projectData)
-            console.log("---")
-            console.log("fetch: " + JSON.stringify(projectData))
-            // Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
-            res.json(projectData);
-        } catch (error) {
-            console.log("fetch error: " + error)
-            // res.status(500).send({ "e": "e" })
-            res.status(500).send(new Error("Server Error: " + error))
-        }
+    app.use(bodyParser.urlencoded({ extended: false })) // not required if using express
+    app.use(bodyParser.json())
 
+    app.use(cors()) // The ‘cors’ package should be installed in the project from the command line, required in the project file server.js, and the instance of the app should be setup to use cors().
 
-        // The API Key variable is passed as a parameter to fetch() .
-    } else {
-        res.status(404).send({ "Request Error": "Empty query param zip=" + zip });
-    }
-})
+    app.listen(port, () => {
+        console.log("\n--- Server Init Start ---")
+        console.log("App is listening: " + "http://localhost:" + port)
+        console.log("Apikey: " + apiKey)
+        console.log("--- Server Init End ---\n")
+    })
 
+    initGetApiKey(apiKey)
+    initGetData()
+    initPost(app)
+
+}
+
+function initGetData() {
+
+    app.get('/getData', async function (req, res) {
+        console.log("--- Get getData Call Start ---")
+        console.log("sending projectData.getData: " + JSON.stringify(projectData.postData))
+        res.status(200).send({ getData: projectData.postData })
+        console.log("--- Get getData Call End ---\n")
+    })
+}
+
+function initGetApiKey(apiKey) {
+
+    app.get('/apiKey', async function (req, res) {
+        console.log("--- Get ApiKey Call Start ---")
+        res.status(200).send({ apiKey })
+        console.log("--- Get ApiKey Call End ---\n")
+    })
+}
+
+function initPost(app) {
+
+    app.post('/post', async function (req, res) {
+        console.log("--- Post Call Start ---")
+        console.log("/post call with req.body: " + JSON.stringify(req.body))
+        console.log("current value of projectData.postData: " + JSON.stringify(projectData.postData))
+        const { date, temp, feelings } = req.body
+        projectData.postData.push({ date, temp, feelings })
+        console.log("new value of projectData.postData: " + JSON.stringify(projectData.postData))
+        res.status(200).send({ success: "true" })
+        console.log("--- Post Call End ---\n")
+    })
+}
 // You should be able to add an entry to the project endpoint using a POST route setup on the server side and executed on the client side as an asynchronous function.
 // The server side function should create a new entry in the apps endpoint (the named JS object) consisting of the data received from the client side POST.
-const data = []
-app.post('/post', async function (req, res) {
-    data.push(req.body)
-    console.log("Data: " + JSON.stringify(data))
-})
--- p3WeatherJournal/static/js/app.js
+++ p3WeatherJournal/static/js/app.js
@@ -1,63 +1,95 @@
 // app.js
 
+let apiKey = ""
+const owmEndpoint = "https://api.openweathermap.org/data/2.5/weather" //?zip=" + zip + ",us&appid="
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
+    getApiKey()
     addEntryHolder()
-    initPostButton()
 
     // Adds an event listener to an existing HTML button from DOM using Vanilla JS.
     // In the file app.js, the element with the id of generate should have an addEventListener() method called on it, with click as the first parameter, and a named callback function as the second parameter.
-    document.getElementById("generate").addEventListener("click", getZipWeather)
+    document.getElementById("generate").addEventListener("click", getZipWeather, false)
 
 });
 
-function initPostButton() {
-    const button = createElementWithId("button", "post")
-    button.innerText = "POST"
-    button.addEventListener("click", function () { post({ time: Date() }) })
-    document.body.appendChild(button)
+async function getApiKey() {
+    fetch("http://localhost:8000/apiKey").then(resp => {
+        resp.json().then(data => {
+            apiKey = data.apiKey
+            console.log("Received apiKey: " + apiKey)
+        })
+    })
 }
 
 // The client side function should take two arguments, the URL to make a POST to, and an object holding the data to POST.
 async function post(data) {
 
-    const response = await fetch("http://localhost:8000/post", {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json', },
-        // Body data type must match "Content-Type" header        
-        body: JSON.stringify(data),
-    });
+    const date = new Date()
+    const temp = data.main.temp
+    const feelings = document.getElementById("feelings").value
+    const postButton = document.getElementById("generate")
 
+    postButton.innerText = "Generating"
+    console.log("Posting: %s, %s, %s", date, temp, feelings)
     try {
+        const response = await fetch("http://localhost:8000/post", {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json', },
+            // Body data type must match "Content-Type" header        
+            body: JSON.stringify({ date, temp, feelings }),
+        });
         const newData = await response.json()
+        postButton.innerText = "Generate Success"
         console.log(newData)
-        return newData
-    } catch (error) { console.log("error", error) }
+
+        getData()
+
+    } catch (error) {
+        postButton.innerText = "Generate Failed"
+        console.log("error", error)
+    }
 }
 
-function getZipWeather() {
+function getData() {
+    fetch("http://localhost:8000/getData").then(resp => {
+        resp.json().then(data => {
+            console.log(data)
+            const serverData = data.getData[data.getData.length - 1]
+            console.log("Received serverData: " + JSON.stringify(serverData))
+            document.getElementById("date").innerHTML = "Date: " + serverData.date
+            document.getElementById("temp").innerHTML = "Temp: " + serverData.temp
+        })
+    })
+}
 
-    document.getElementById("zip").value
+async function getZipWeather() {
 
-    if (parseInt(zip) != NaN && zip.length == 5) {
+    zip = document.getElementById("zip").value
+    console.log("zip:" + zip)
 
-        console.log("zip: " + zip);
+    if (typeof zip != "number" && zip.length == 5) {
 
-        let url = "http://localhost:8000/weather?" + new URLSearchParams({ zip })
+        let url = owmEndpoint + "?" + new URLSearchParams({ zip }) + ",us&appid=" + apiKey // The API Key variable is passed as a parameter to fetch() .
+        console.log(url)
+        try {
+            let resp = await fetch(url)
+            const json = await resp.json() // Data is successfully returned from the external API.
+            console.log(json)
+            console.log("fetch data: " + JSON.stringify(json))
+            // Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
+            if (json.cod == 404) throw Error("city not found")
+            post(json)
 
-        fetch(url).then(resp => { // There should be an asynchronous function to fetch the data from the app endpoint
-            resp.json().then(data => {
+        } catch (error) {
+            console.log("fetch error: " + error)
+            alert(error)
 
-                console.log("fetch: " + JSON.stringify(data))
-                // Included in the async function to retrieve that app’s data on the client side, existing DOM elements should have their innerHTML properties dynamically set according to data returned by the app route.
-                document.getElementById("temp").innerHTML = "Temp: " + data.main.temp
-                document.getElementById("date").innerHTML = "Date: " + new Date()
-
-            }).catch(error => { alert("Parse error: " + error) });
-        }).catch(error => { alert("fetch error: " + error) })
+        }
     }
-    else { alert("default GET zip client query value") }
+    else { alert("input error for GET weather client query, zip value of: " + zip) }
 }
 
 function createElementWithId(tag, id) {
